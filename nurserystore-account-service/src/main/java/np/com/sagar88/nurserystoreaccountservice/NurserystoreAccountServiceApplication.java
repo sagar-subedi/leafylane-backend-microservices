c2add2aff14d5131d6dce3e4b6c531829cb3e536
Sagar Subedi <sagarxic@gmail.com>
2025-02-09 12:39:25 +0545

    Enabled feign clients

--- nurserystore-account-service/src/main/java/np/com/sagar88/nurserystoreaccountservice/NurserystoreAccountServiceApplication.java
+++ nurserystore-account-service/src/main/java/np/com/sagar88/nurserystoreaccountservice/NurserystoreAccountServiceApplication.java
@@ -3,9 +3,10 @@ package np.com.sagar88.nurserystoreaccountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableFeignClients
+@EnableFeignClients
 @EnableDiscoveryClient
 public class NurserystoreAccountServiceApplication {
 
