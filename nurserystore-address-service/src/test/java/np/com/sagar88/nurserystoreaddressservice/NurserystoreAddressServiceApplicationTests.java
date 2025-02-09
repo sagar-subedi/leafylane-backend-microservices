173bf7a166867e292f117d67a4dd315e8a9cdc22
Sagar Subedi <sagarxic@gmail.com>
2025-02-09 15:54:32 +0545

    renamed parent package

--- nurserystore-address-service/src/test/java/np/com/sagar88/nurserystoreaddressservice/NurserystoreAddressServiceApplicationTests.java
+++ nurserystore-address-service/src/test/java/np/com/sagar88/nurserystoreaddressservice/NurserystoreAddressServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package np.com.sagar88.nurserystoreeurekadiscoveryservice;
+package np.com.sagar88.nurserystoreaddressservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NurserystoreBillingServiceApplicationTests {
+class NurserystoreAddressServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
