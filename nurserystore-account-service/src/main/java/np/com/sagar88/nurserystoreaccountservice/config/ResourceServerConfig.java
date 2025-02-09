9d72a70906dbfae811e14675b294c79d43e7b00a
Sagar Subedi <sagarxic@gmail.com>
2025-02-09 12:38:26 +0545

    Configure account as resource server to set role as authority in authentication (security context)

--- nurserystore-account-service/src/main/java/np/com/sagar88/nurserystoreaccountservice/config/ResourceServerConfig.java
+++ nurserystore-account-service/src/main/java/np/com/sagar88/nurserystoreaccountservice/config/ResourceServerConfig.java
@@ -1,5 +1,6 @@
 package np.com.sagar88.nurserystoreaccountservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -16,13 +17,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ResourceServerConfig {
 
+    @Autowired
+    CustomJwtAuthenticationConverter customJwtAuthenticationConverter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/resource/test/**").hasAuthority("SCOPE_message.read")
                 .requestMatchers("/**").authenticated())
-                .oauth2ResourceServer()
-                .jwt();
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter)
+                ));
         return http.build();
     }
 }
