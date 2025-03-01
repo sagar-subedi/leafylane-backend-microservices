ef38acfef15ba3d6cc9290139fa2de61637745f5
Sagar Subedi <sagarxic@gmail.com>
2025-03-01 09:50:08 +0545

    Configure api gateway service

---
+++ nurserystore-api-gateway-service/src/main/java/np/com/sagar88/nurserystoreapigatewayservice/config/GatewayConfig.java
@@ -0,0 +1,26 @@
+package np.com.sagar88.nurserystoreapigatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // Set allowed origin
+        corsConfig.setAllowedMethods(Collections.singletonList("*")); // Allow all methods
+        corsConfig.setAllowedHeaders(Collections.singletonList("*")); // Allow all headers
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
